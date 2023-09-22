e52cbe0f9db0746aca38d6ab764f087710e4fd5f
Sahil181045 <sahilkadam257@gmail.com>
2023-09-22 19:26:18 +0530

    first commit

---
+++ Python/test.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+window = Tk()
+
+btn1 = Button(text=" ", width=10, height=3).grid(
+    row=0, column=0, padx=1, pady=1)
+btn2 = Button(text=" ", width=10, height=3).grid(
+    row=0, column=1, padx=1, pady=1)
+btn3 = Button(text=" ", width=10, height=3).grid(
+    row=0, column=2, padx=1, pady=1)
+btn4 = Button(text=" ", width=10, height=3).grid(
+    row=1, column=0, padx=1, pady=1)
+btn5 = Button(text=" ", width=10, height=3).grid(
+    row=1, column=1, padx=1, pady=1)
+btn6 = Button(text=" ", width=10, height=3).grid(
+    row=1, column=2, padx=1, pady=1)
+btn7 = Button(text=" ", width=10, height=3).grid(
+    row=2, column=0, padx=1, pady=1)
+btn8 = Button(text=" ", width=10, height=3).grid(
+    row=2, column=1, padx=1, pady=1)
+btn9 = Button(text=" ", width=10, height=3).grid(
+    row=2, column=2, padx=1, pady=1)
+
+window.mainloop()
